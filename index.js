f9397f1ea58cd1501748be146e80ad4d00ff7cb9
SETHMCOPLEN <47903452+SETHMCOPLEN@users.noreply.github.com>
2025-11-21 18:51:56 -0500

    Simplify server running log message

--- index.js
+++ index.js
@@ -33,4 +33,4 @@ app.get('/api/passes', async (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Server running on port ' + port));
+app.listen(port, () => console.log('Server running'));
